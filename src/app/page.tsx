3d116581c76ffc1a5be42547565efac6f2e50fce
Subhadeep-CS <dassubhadeep631@gmail.com>
2025-06-06 21:52:25 +0530

    routing to dashboard logic added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,30 @@
-"use client";
 import { SignInButton, useUser } from "@clerk/nextjs";
-import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function Home() {
   const { isSignedIn } = useUser();
+  const router = useRouter();
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push("/dashboard");
+    }
+  }, [isSignedIn]);
+
   return (
     <div className="flex items-center justify-center h-screen bg-black text-white">
       <div className="text-center max-w-[800px] !space-y-4">
         <h1 className="text-5xl font-bold">
           WELCOME TO WHITE BOARD APPLICATION!
         </h1>
-        {/* Subtitle */}
         <p className="text-gray-300 mt-4 text-lg">
           YOU CAN CREATE,EDIT YOU OWN DESIGN
         </p>
-
-        {/* Button with hover effect */}
-        {isSignedIn ? (
-          <Link
-            href="/white-board"
-            className="mt-6 inline-block bg-green-600 hover:bg-green-700 transition-all text-white text-lg font-semibold px-16 py-3 rounded-full"
-          >
-            Go to Whiteboard →
-          </Link>
-        ) : (
-          <SignInButton mode="modal">
-            <button className="mt-6 inline-block bg-indigo-600 hover:bg-indigo-700 transition-all text-white text-lg font-semibold px-16 py-3 rounded-full">
-              SIGN IN TO CONTINUE →
-            </button>
-          </SignInButton>
-        )}
+        <SignInButton mode="modal">
+          <button className="mt-6 inline-block bg-indigo-600 hover:bg-indigo-700 transition-all text-white text-lg font-semibold px-16 py-3 rounded-full">
+            SIGN IN TO CONTINUE →
+          </button>
+        </SignInButton>
       </div>
     </div>
   );
