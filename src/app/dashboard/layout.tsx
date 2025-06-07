b5cbab6a0ae4bc04684b6c4a389e7ec502283818
Subhadeep-CS <dassubhadeep631@gmail.com>
2025-06-07 15:09:54 +0530

    completed

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,9 +6,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <>
       <NavBar />
-      <body>{children}</body>
-    </html>
+      {children}
+    </>
   );
 }
